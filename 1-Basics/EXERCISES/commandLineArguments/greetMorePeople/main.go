8a691d604f4c5fee6245dd5bf4ca02d09f40be1e
Felipe <fas.felipesantos@gmail.com>
2023-11-09 20:16:41 -0300

    countArguments done

---
+++ 1-Basics/EXERCISES/commandLineArguments/greetMorePeople/main.go
@@ -0,0 +1,50 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Greet More People
+//
+// RESTRICTIONS
+//  1. Be sure to match the expected output below
+//  2. Store the length of the arguments in a variable
+//  3. Store all the arguments in variables as well
+//
+// INPUT
+//  bilbo balbo bungo
+//
+// EXPECTED OUTPUT
+//  There are 3 people!
+//  Hello great bilbo !
+//  Hello great balbo !
+//  Hello great bungo !
+//  Nice to meet you all.
+// ---------------------------------------------------------
+
+func main() {
+	// TYPE YOUR CODE HERE
+
+	// BONUS #1:
+	// Observe the error if you pass less then 3 arguments.
+	// Search on the web how to solve that.
+
+	people := os.Args
+	people = people[1:]
+	count := len(people)
+
+	fmt.Printf("There are %d people.\n", count)
+	for i := 0; i < count; i++ {
+		fmt.Printf("Hello great %s.\n", people[i])
+	}
+}
---
+++ 1-Basics/EXERCISES/commandLineArguments/printThePath/main.go
@@ -0,0 +1,39 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Print the Path
+//
+//  Print the path of the running program by getting it
+//  from `os.Args` variable.
+//
+// HINT
+//  Use `go build` to build your program.
+//  Then run it using the compiled executable program file.
+//
+// EXPECTED OUTPUT SHOULD INCLUDE THIS
+//  myprogram
+// ---------------------------------------------------------
+
+func main() {
+	var dir, file string
+	dir, file = path.Split(os.Args[0])
+	fmt.Println(dir, file)
+}
+
+// func main() {
+// 	fmt.Println(os.Args[0])
+// }
