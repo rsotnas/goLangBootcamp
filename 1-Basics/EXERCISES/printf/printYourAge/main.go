f219d7aa052027dd266975c6119540c18cf1cc42
Felipe <fas.felipesantos@gmail.com>
2023-11-11 23:19:06 -0300

    printf exercises done

---
+++ 1-Basics/EXERCISES/printf/printYourAge/main.go
@@ -0,0 +1,37 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Print Your Fullname
+//
+//  1. Get your name and lastname from the command-line
+//  2. Print them using Printf
+//
+// EXAMPLE INPUT
+//  Inanc Gumus
+//
+// EXPECTED OUTPUT
+//  Your name is Inanc and your lastname is Gumus.
+// ---------------------------------------------------------
+
+func main() {
+	// BONUS: Use a variable for the format specifier
+	var name, lastname, text string
+	input := os.Args[1:]
+	name = input[0]
+	lastname = input[1]
+	text = "Your name is %s and your last name is %s.\n"
+	fmt.Printf(text, name, lastname)
+}
