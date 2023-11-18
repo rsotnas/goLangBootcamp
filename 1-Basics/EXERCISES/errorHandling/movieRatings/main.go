dc89c8e13533166e492c1646a6b8beaf1a2bf581
Felipe <fas.felipesantos@gmail.com>
2023-11-18 14:25:20 -0300

    error handling exercises done

---
+++ 1-Basics/EXERCISES/errorHandling/movieRatings/main.go
@@ -0,0 +1,88 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// ---------------------------------------------------------
+// STORY
+//
+//  Your boss wants you to create a program that will check
+//  whether a person can watch a particular movie or not.
+//
+//  Imagine that another program provides the age to your
+//  program. Depending on what you return, the other program
+//  will issue the tickets to the person automatically.
+//
+// EXERCISE: Movie Ratings
+//
+//  1. Get the age from the command-line.
+//
+//  2. Return one of the following messages if the age is:
+//     -> Above 17         : "R-Rated"
+//     -> Between 13 and 17: "PG-13"
+//     -> Below 13         : "PG-Rated"
+//
+// RESTRICTIONS:
+//  1. If age data is wrong or absent let the user know.
+//  2. Do not accept negative age.
+//
+// BONUS:
+//  1. BONUS: Use if statements only twice throughout your program.
+//  2. BONUS: Use an if statement only once.
+//
+// EXPECTED OUTPUT
+//  go run main.go 18
+//    R-Rated
+//
+//  go run main.go 17
+//    PG-13
+//
+//  go run main.go 12
+//    PG-Rated
+//
+//  go run main.go
+//    Requires age
+//
+//  go run main.go -5
+//    Wrong age: "-5"
+// ---------------------------------------------------------
+
+func main() {
+
+	args := os.Args
+	var age int
+	var err error
+
+	if len(args) != 2 {
+		fmt.Println("Requires age")
+		return
+	} else {
+		age, err = strconv.Atoi(args[1])
+		// fmt.Println(age, err)
+		if err != nil || age <= 0 {
+			fmt.Printf("Wrong info: %d. Error: %q\n", age, err)
+			return
+		}
+		if age > 17 {
+			fmt.Println("R-Rated")
+		} else if age > 13 {
+			fmt.Println("PG-13")
+		} else if age < 13 {
+			fmt.Println("PG-Rated")
+		} else {
+			fmt.Println(err)
+		}
+	}
+
+}
