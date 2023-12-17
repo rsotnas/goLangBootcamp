f946ca9c6148996c8385138960aee4e3ea5cb655
Felipe <fas.felipesantos@gmail.com>
2023-12-16 21:13:47 -0300

    .

---
+++ 2-ForGoBasics/EXERCISES/labeledStatements/crunchThePrimes/main.go
@@ -0,0 +1,79 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+// ---------------------------------------------------------
+// EXERCISE: Crunch the primes
+//
+//  1. Get numbers from the command-line.
+//  2. `for range` over them.
+//  4. Convert each one to an int.
+//  5. If one of the numbers isn't an int, just skip it.
+//  6. Print the ones that are only the prime numbers.
+//
+// RESTRICTION
+//  The user can run the program with any number of
+//  arguments.
+//
+// HINT
+//  Find whether a number is prime using this algorithm:
+//  https://stackoverflow.com/a/1801446/115363
+//
+// EXPECTED OUTPUT
+//  go run main.go 2 4 6 7 a 9 c 11 x 12 13
+//    2 7 11 13
+//
+//  go run main.go 1 2 3 5 7 A B C
+//    2 3 5 7
+// ---------------------------------------------------------
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// remember [1:] skips the first argument
+
+main:
+	for _, arg := range os.Args[1:] {
+		n, err := strconv.Atoi(arg)
+		if err != nil {
+			// skip non-numerics
+			continue
+		}
+
+		switch {
+		// prime
+		case n == 2 || n == 3:
+			fmt.Print(n, " ")
+			continue
+
+		// not a prime
+		case n <= 1 || n%2 == 0 || n%3 == 0:
+			continue
+		}
+
+		for i, w := 5, 2; i*i <= n; {
+			// not a prime
+			if n%i == 0 {
+				continue main
+			}
+
+			i += w
+			w = 6 - w
+		}
+
+		// all checks ok: it's a prime
+		fmt.Print(n, " ")
+	}
+
+	fmt.Println()
+}
